d7ecc8e7caca4118c88e1c40f989e9f0109d23cd
tfennelly <tom.fennelly@gmail.com>
2015-07-22 09:25:02 +0100

    Don't pass window to test funk. If functions need it, they can get the window via the window-handle

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ exports.onPage = function(testFunc, content) {
             require("window-handle").setWindow(window);
             
             // A new "window" instance is created for each test, so we need to clear anything
-            // that could be bound to that, forcing recreate for the new window instance.
+            // that could be created from that, forcing recreate for the new window instance.
             require("jquery-detached-2.1.4").clearSharedJQuery();
             require("jqueryui-detached-1.11").clear();
             require("bootstrap-detached-3.3").clear();
             
-            testFunc(window);
+            testFunc();
         }
     );    
 }
