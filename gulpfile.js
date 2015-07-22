73555a1126231d3dad1683a212c482ad9374bcec
tfennelly <tom.fennelly@gmail.com>
2015-07-22 13:32:53 +0100

    Change build to use jenkins-js-builder

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,3 @@
-var gulp = require('gulp');
-var jasmine = require('gulp-jasmine');
-var jasmineReporters = require('jasmine-reporters');
+var builder = require('jenkins-js-builder');
 
-global.jenkins = {
-    src: '/a/b/c'
-};
-
-gulp.task('default', function () {
-    return gulp.src('spec/*-spec.js')
-        .pipe(jasmine({
-            reporter: new jasmineReporters.TerminalReporter({
-                verbosity: 3,
-                color: true,
-                showStack: true
-            })
-        }));
-});
+builder.defineTasks();
